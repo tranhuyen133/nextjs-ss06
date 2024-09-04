37302bba34f33ca33b2a087cea09e9598bb4a885
tranhuyen133 <147359382+tranhuyen133@users.noreply.github.com>
2024-09-04 15:06:39 +0700

    06

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import Button from "../../../components/Button"
+import Button from "@/components/Button"
+export default function DashBoard() {
+    return (
+        <div>
+            đây là trang dashboard!
+            <Button></Button>
+        </div>
+    )
+}
---
+++ app/(user)/profile/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div>trang thông tin user</div>
+    )
+}
